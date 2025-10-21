be7b0a4d9b45f50649e069166e41ef16f4e34126
maheshparajuli <maheshparajuli717@gmail.com>
2025-10-21 10:58:02 +0545

    gg

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -7,6 +7,9 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
+from argon2 import PasswordHasher
+from argon2.exceptions import HashingError
+
 
 """   
 
@@ -26,5 +29,28 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register',methods=('GET',"POST"))
 def register():
     if request.method == 'POST':
-        usrname=request.form['username']
-        
+        username=request.form['username']
+        password=request.form['password']
+        db=get_db()
+        error=None
+
+        if not username:
+            error='username is required'
+        elif not password:
+            error='password is required'
+        elif len(password)<8:
+            error='password must be at least 8 characters'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username,password) VALUES(?,?)",(username,ph.hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error=f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+        flash(error)
+    return render_template('auth/register.html')
+
